fa11a249850606d455105e07e7483da7ac82bb56
Rajasekaran Radhakrishnan <rrajsekarm@gmail.com>
2016-10-13 14:02:31 +0530

    updated server.js
    updated server.js

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 var http = require('http');
+
+var express = require('express'),
+    fs      = require('fs'),
+    app     = express(),
+    eps     = require('ejs'),
+    morgan  = require('morgan');
+    
+Object.assign=require('object-assign')
+
+app.engine('html', require('ejs').renderFile);
+app.use(morgan('combined'))
+
+
 //loads http module
 var app=http.createServer(function (req, res) {
 //creates server
   res.writeHead(200, {'Content-Type': 'text/plain'});
+  var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+
   //sets the right header and status code
   res.end('Hello World\n');
   //outputs string with line end symbol
-}).listen(1337, "127.0.0.1");
-//sets port and IP address of the server
-console.log('Server running at http://127.0.0.1:1337/');
+})
